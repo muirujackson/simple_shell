e547bdd19f2e76e766717e8114e72d1d73ee844f
Brian Aslachid <aslachidewei@gmail.com>
2023-05-15 03:02:20 -0700

    main file

---
+++ main.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(int argc,char*argv[])
+{
+        printf("Command-line arguments:\n");
+                for(int i=0;i<argc;i++)
+                {
+                        printf("Arguments%d:%s\n,i,argv[i]");
+                }
+                return0;
+}
