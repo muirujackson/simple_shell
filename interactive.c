717ec15d31051ea8bf768b9cb792e083d98c7484
muirujackson <jakezweru@gmail.com>
2023-05-18 06:22:10 -0400

    readline

---
+++ interactive.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * interactive_mode - function for interactive mode
+ *
+ * Return: void
+ */
+void interactive_mode(void)
+{
+	char *line;
+	char *args;
+	while (1)
+	{
+		write(0,"$ ",2);
+		line = read_line();
+		printf("%s",line);
+	 }
+}
+char *read_line(void)
+{
+	char *buffer = NULL;
+	size_t buffer_size = 0;
+	ssize_t bytes_read;
+
+	bytes_read = getline(&buffer, &buffer_size, stdin);
+	if (bytes_read == -1)
+	{
+		if(feof(stdin))
+		{
+			free(buffer);
+			exit(EXIT_SUCCESS);
+		} else
+		{
+			free(buffer);
+			/* Error occurred while reading the line */
+			perror("Error reading standard input");
+			exit(EXIT_FAILURE);
+		}
+	}
+	return (buffer);
+}
+
+char **split_strings(char *)
+{
+
+
+}
