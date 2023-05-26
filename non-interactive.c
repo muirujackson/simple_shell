c018b2e9dcd4f7f867f88c7281bc85fd1ae53db6
muirujackson <jakezweru@gmail.com>
2023-05-26 16:59:25 -0700

    solve memory leak

--- non-interactive.c
+++ non-interactive.c
@@ -18,11 +18,11 @@ void non_interactive_mode(char *name)
 		line = read_input();
 		args = split_strings(line);
 		execute_status = execute_command(args, name);
+		/* avoid memory leaks */
+		free(line);
+		free(args);
 		if (execute_status >= 0)
 		{
-			/* avoid memory leaks */
-			free(line);
-			free(args);
 			exit(execute_status);
 		} else
 			continue;
